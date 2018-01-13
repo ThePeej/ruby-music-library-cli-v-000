2d54a75256efe26d9e952d3aabb6ca31db9bd1c3
PJ Frias <peejfrias@gmail.com>
2018-01-13 19:04:06 +0000

    automatically backed up by learn

--- lib/Songs.rb
+++ lib/Songs.rb
@@ -1,5 +1,11 @@
 class Songs
+attr_accessor :name, :artist, :genre
 
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+    @artist = artist if artist
+    @genre = genre if genre
+  end
 
 
 end
