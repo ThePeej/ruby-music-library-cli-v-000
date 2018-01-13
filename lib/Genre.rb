817c81076308e1d1dd36cfb18cdb4450871a9606
PJ Frias <peejfrias@gmail.com>
2018-01-13 19:18:03 +0000

    automatically backed up by learn

---
+++ lib/Genre.rb
@@ -0,0 +1,20 @@
+class Genre
+  extend Persistable::ClassMethods
+  include Persistable::InstanceMethods
+
+
+  attr_accessor :name, :songs, :genre
+
+  @@all = []
+
+
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+end
