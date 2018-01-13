12d0edffa8d1db2b6fe39c257ac6be2ea16e54e2
PJ Frias <peejfrias@gmail.com>
2018-01-13 19:15:16 +0000

    automatically backed up by learn

--- lib/Songs.rb
+++ lib/Songs.rb
@@ -13,16 +13,4 @@ class Song
     @@all
   end
 
-  def self.destroy_all
-    self.all.clear
-  end
-
-  def save
-    self.class.all << self
-  end
-
-  def self.create(name)
-    self.new(name).tap{|s| s.save}
-  end
-
 end
